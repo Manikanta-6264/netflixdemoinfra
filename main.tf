e9131aee442267651a59a01723c1ee051f4d8be4
Manikanta-6264 <manikantagembali1@gmail.com>
2025-04-03 15:47:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 
 resource "aws_instance" "one" {
 count = 5
-ami = "ami-0ddc798b3f1a5117e"
+ami = "ami-0a9a48ce4458e384e"
 instance_type = "t2.medium"
 key_name = "swikp"
 vpc_security_group_ids = ["sg-05f044979e305302e"]
